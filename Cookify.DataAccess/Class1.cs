2d2386d7e765d1fd8ac436cbff112ca6c0579786
Adrian Kurek <adrian.kurek93@gmail.com>
2021-06-03 12:52:31 +0200

    Add application database context

--- Cookify.DataAccess/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Cookify.DataAccess
-{
-	public class Class1
-	{
-	}
-}
---
+++ Cookify.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Cookify.Models;
+
+namespace Cookify.DataAccess.Data
+{
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+
+		}
+		
+		public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+		public DbSet<Comment> Comment { get; set; }
+		public DbSet<FavoriteRecipe> FavoriteRecipe { get; set; }
+		public DbSet<Image> Image { get; set; }
+		public DbSet<ImageRecipe> ImageRecipe { get; set; }
+		public DbSet<Ingredient> Ingredient { get; set; }
+		public DbSet<IngredientRecipe> IngredientRecipe { get; set; }
+		public DbSet<Recipe> Recipe { get; set; }
+		public DbSet<RecipeCategory> RecipeCategory { get; set; }
+		public DbSet<Step> Step { get; set; }
+	}
+}
