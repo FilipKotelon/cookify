438cc68b0ce28d6a81840641b4730868262c22ae
Adrian Kurek <adrian.kurek93@gmail.com>
2021-06-04 20:03:16 +0200

    Implement Unit of Work design pattern

---
+++ Cookify.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Cookify.DataAccess.Repository.IRepository
+{
+	public interface IUnitOfWork : IDisposable
+	{
+		ApplicationUserRepository ApplicationUser { get; }
+		CommentRepository Comment { get; }
+		FavoriteRecipeRepository FavoriteRecipe { get; }
+		ImageRecipeRepository ImageRecipe { get; }
+		ImageRepository Image { get; }
+		IngredientRecipeRepository IngredientRecipe { get; }
+		IngredientRepository Ingredient { get; }
+		RecipeCategoryRepository RecipeCategory { get; }
+		RecipeRepository Recipe { get; }
+		StepRepository Step { get; }
+		void Save();
+	}
+}
