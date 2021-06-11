a7fd0dbdd0a03107f263d19a10bcb10b3fb8131a
Blake Greenleaf <off.f.kotelon@gmail.com>
2021-06-11 19:20:53 +0200

    Test

--- Cookify.Web/wwwroot/ts/components/components.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logTheForbiddenWords = void 0;
-exports.logTheForbiddenWords = () => {
-    console.log('Hello world ! ! ! ! ! :)');
-};
-//# sourceMappingURL=components.js.map
--- Cookify.Web/wwwroot/ts/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("../scss/main.scss");
-const components_1 = require("./components/components");
-console.log('works definitely');
-components_1.logTheForbiddenWords();
-//# sourceMappingURL=index.js.map
