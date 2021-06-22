326873fe4ebd3e0ee71563e48daa26c3080ec398
Adrian Kurek <adrian.kurek93@gmail.com>
2021-06-22 21:22:38 +0200

    Add basic controller and view for recipes

---
+++ Cookify.Web/Controllers/RecipeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Cookify.DataAccess.Repository.IRepository;
+
+namespace Cookify.Controllers
+{
+    public class RecipeController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public RecipeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            var recipes = _unitOfWork.Recipe.GetAll(includeProperties: "RecipeCategory");
+            return View(recipes);
+        }
+    }
+}
