5fc243fe9962b51c4f93ca37d3fb5b08904bee69
Blake Greenleaf <off.f.kotelon@gmail.com>
2021-06-23 16:08:44 +0200

    Setup frontowy

--- Cookify.Web/wwwroot/ts/components/favorite-heart/favorite-heart.class.ts
+++ Cookify.Web/wwwroot/ts/components/favorite-heart/favorite-heart.class.ts
@@ -1,14 +1,36 @@
 export class FavoriteHeart{
-  constructor(){
+  el: HTMLElement;
+  filled: boolean;
 
+  constructor(checked = false){
+    this.filled = checked;
+  }
+
+  init(){
+    this.setupOnFill();
+    this.setupOnEmpty();
+  }
+
+  setupOnFill(){
+    this.el.addEventListener('click', () => {
+      this.addToFavorites();
+      this.fillHeart();
+    })
+  }
+
+  setupOnEmpty(){
+    this.el.addEventListener('click', () => {
+      this.removeFromFavorites();
+      this.emptyHeart();
+    })
   }
 
   fillHeart(){
-
+    this.el.classList.add('filled');
   }
 
   emptyHeart(){
-    
+    this.el.classList.remove('filled');
   }
 
   addToFavorites(){
