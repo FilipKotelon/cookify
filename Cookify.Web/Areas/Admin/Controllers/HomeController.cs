a3b87150655dc5c619417f21ea44e0307c84ee10
Blake Greenleaf <off.f.kotelon@gmail.com>
2021-06-25 20:37:55 +0200

    Organizing admin panel

--- Cookify.Web/Areas/Admin/Controllers/HomeController.cs
+++ Cookify.Web/Areas/Admin/Controllers/HomeController.cs
@@ -7,9 +7,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Cookify.Areas.User.Controllers
+namespace Cookify.Web.Areas.Admin.Controllers
 {
-    [Area("User")]
+    [Area("Admin")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
--- Cookify.Web/Areas/User/Controllers/RecipeCategoryController.cs
+++ Cookify.Web/Areas/User/Controllers/RecipeCategoryController.cs
@@ -19,58 +19,5 @@ namespace Cookify.Areas.User.Controllers
             var recipeCategories = _unitOfWork.RecipeCategory.GetAll();
             return View(recipeCategories);
         }
-
-        public IActionResult Upsert(int? id)
-        {
-            RecipeCategory recipeCategory = new RecipeCategory();
-
-            if (id == null)
-            {
-                return View(recipeCategory);
-            }
-
-            recipeCategory = _unitOfWork.RecipeCategory.Get(id.GetValueOrDefault());
-
-            if (recipeCategory == null)
-            {
-                return NotFound();
-            }
-
-            return View(recipeCategory);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Upsert(RecipeCategory recipeCategory)
-        {
-            if (ModelState.IsValid)
-            {
-                if (recipeCategory.Id == 0)
-                {
-                    _unitOfWork.RecipeCategory.Add(recipeCategory);
-
-                }
-                else
-                {
-                    _unitOfWork.RecipeCategory.Update(recipeCategory);
-                }
-
-                _unitOfWork.Save();
-
-                return RedirectToAction(nameof(Index));
-            }
-
-            return View(recipeCategory);
-        }
-
-        public IActionResult Delete(int id)
-        {
-            var objFromDb = _unitOfWork.RecipeCategory.Get(id);
-
-            _unitOfWork.RecipeCategory.Remove(objFromDb);
-            _unitOfWork.Save();
-
-            return RedirectToAction(nameof(Index));
-        }
     }
 }
