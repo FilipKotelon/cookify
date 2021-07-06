9feffa706fa8f197a06a4d17a171e02958bd4b1d
uranowski-wsei <antoni.uranowski@microsoft.wsei.edu.pl>
2021-07-06 02:37:06 +0200

    added user login view

---
+++ Cookify.Web/Areas/User/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cookify.Web.Areas.User.Controllers
+{
+    [Area("User")]
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult FavoriteRecepies()
+        {
+            return View();
+        }
+
+        public IActionResult Settings()
+        {
+            return View();
+        }
+
+        public IActionResult SendRecepie()
+        {
+            return View();
+        }
+    }
+}
