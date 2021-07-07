55f186b6d8456771efe842d1b0cf0b5b5c3921f0
Adrian Kurek <adrian.kurek93@gmail.com>
2021-07-07 20:22:18 +0200

    Add list of favorite recipes

---
+++ Cookify.Web/Areas/User/Controllers/FavoriteRecipeController.cs
@@ -0,0 +1,35 @@
+﻿using Cookify.DataAccess.Repository.IRepository;
+using Cookify.Models;
+using Cookify.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
+using System.Security.Claims;
+using System.Collections.Generic;
+
+namespace Cookify.Areas.User.Controllers
+{
+    [Area("User")]
+    public class FavoriteRecipeController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public FavoriteRecipeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var currentUserId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            var favoriteRecipes = _unitOfWork.FavoriteRecipe.GetAll(recipe => recipe.ApplicationUserId.Equals(currentUserId));
+            IEnumerable<int> favoriteRecipeIds = favoriteRecipes.Select(i => i.RecipeId);
+
+            var recipes = _unitOfWork.Recipe.GetAll(recipe => favoriteRecipeIds.Contains(recipe.Id));
+            
+            return View(recipes);
+        }
+    }
+}
